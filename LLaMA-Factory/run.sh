53cd441915276f3f498016190f05c93558963349
zywu <361456446@qq.com>
2025-01-10 09:59:29 +0800

    fix sft

--- LLaMA-Factory/run.sh
+++ LLaMA-Factory/run.sh
@@ -1,11 +1,11 @@
 yaml=$1
 export HF_ENDPOINT="https://hf-mirror.com"
-export HOME="/cpfs01/user/chenyinghao"
-export HF_HOME="/cpfs01/user/chenyinghao/.cache"
+export HOME="/cpfs01/user/wuzhiyong"
+export HF_HOME="/cpfs01/user/wuzhiyong/.cache"
 export FORCE_TORCHRUN=1
-source /cpfs01/user/chenyinghao/anaconda3/etc/profile.d/conda.sh
-
-conda activate llama_fac
-cd /cpfs01/user/chenyinghao/WebRL/LLaMA-Factory
+source /cpfs01/user/wuzhiyong/anaconda3/etc/profile.d/conda.sh
+/root/anaconda3/etc/profile.d/conda.sh
+conda activate fac
+cd /cpfs01/user/wuzhiyong/rl/LLaMA-Factory
 echo ${yaml}
 llamafactory-cli train ${yaml}
